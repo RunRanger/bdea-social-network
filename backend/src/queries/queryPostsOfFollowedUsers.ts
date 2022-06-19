8787bee35efcc6f95f9b33df3b317411d4ad2583
RunRanger <jan.e.mayer@web.de>
2022-06-19 20:51:00 +0200

    +add documents

--- backend/src/queries/queryPostsOfFollowedUsers.ts
+++ backend/src/queries/queryPostsOfFollowedUsers.ts
@@ -6,22 +6,22 @@ const queryPostsOfFollowedUsers = async (db: Database, userId: string, mode: 'ne
     return new Promise((resolve, reject) => {
         db.query(`
         LET USERS = (
-          FOR f IN follows
-               FILTER f._from == '${"users/" +userId}'
-               RETURN f._to
-          )
-          
-          LET followedTweets = (
-          FOR w IN wrote
-              FILTER w._from IN USERS
-              RETURN w._to
-          )
-          
-          FOR t IN tweets
-              FILTER t._id IN followedTweets
-              SORT ${sort} DESC
-              ${limit !== -1 ? `LIMIT ${limit}` : ''}
-              RETURN t
+        FOR f IN follows
+              FILTER f._from == '${"users/" +userId}'
+              RETURN f._to
+        )
+        
+        LET followedTweets = (
+        FOR w IN wrote
+            FILTER w._from IN USERS
+            RETURN w._to
+        )
+        
+        FOR t IN tweets
+            FILTER t._id IN followedTweets
+            SORT ${sort} DESC
+            ${limit !== -1 ? `LIMIT ${limit}` : ''}
+            RETURN t
     `).then(result => resolve(result.all())).catch(e => reject(e))
     });
 }
