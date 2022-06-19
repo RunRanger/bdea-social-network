702c41f8c19da4ff3df134819cba40cdecee72d7
lvckylvkas <julian10000001@googlemail.com>
2022-06-19 20:55:00 +0200

    limiter BACK!

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { runInitalLoad } from './initalLoad/initalLoad';
 import startWebservice from './startWebservice';
 
 const main = async () => {
-  const db = await runInitalLoad(-1); // -1 means no limit, otherwise the number of tweets to load
+  const db = await runInitalLoad(1000); // -1 means no limit, otherwise the number of tweets to load
 
   startWebservice(db);
 }
